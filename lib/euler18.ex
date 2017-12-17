c0105892f66b3d99d075fa0de0164d42a8570d2a
Rubén Chavarría <txingo@gmail.com>
2017-12-17 20:25:14 +0100

    Just create a new `mix` project to start solving the Euler18 exercise

---
+++ lib/euler18.ex
@@ -0,0 +1,18 @@
+defmodule Euler18 do
+  @moduledoc """
+  Documentation for Euler18.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Euler18.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/euler18_test.exs
@@ -0,0 +1,8 @@
+defmodule Euler18Test do
+  use ExUnit.Case
+  doctest Euler18
+
+  test "greets the world" do
+    assert Euler18.hello() == :world
+  end
+end
