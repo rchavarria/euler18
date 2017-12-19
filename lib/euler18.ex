8120bbd530408213ec61a822f79753fe5638a17a
Rubén Chavarría <txingo@gmail.com>
2017-12-19 22:17:51 +0100

    Look the max value of just the two values under the last one
    Instead of looking max value in the whole row, we start with the row, drop as many columns as the column of the last value, take just 2 values, and compute the max of them.
    
    Then, find the index of that max value (it can be just 0 or 1).
    
    Next iteration, the column index is the last one plus the index of the current max value.

--- lib/euler18.ex
+++ lib/euler18.ex
@@ -1,10 +1,17 @@
 defmodule Euler18 do
 
-  def find_max_sum([]), do: 0
-  def find_max_sum([[]]), do: 0
-  def find_max_sum([ top | rows ]) do
-    max_top = top |> Enum.max()
-    max_top + find_max_sum(rows)
+  def find_max_sum(piramid) do
+    sum(piramid, 0)
+  end
+
+  defp sum([], _col), do: 0
+  defp sum([[]], _col), do: 0
+  defp sum([ top | rows ], col) do
+    meaningful_cols = top |> Enum.drop(col) |> Enum.take(2)
+    max_top = meaningful_cols |> Enum.max()
+    col_of_max = meaningful_cols |> Enum.find_index(&(&1 == max_top))
+
+    max_top + sum(rows, col + col_of_max)
   end
 
 end
--- test/euler18_test.exs
+++ test/euler18_test.exs
@@ -2,14 +2,17 @@ defmodule Euler18Test do
   use ExUnit.Case
   doctest Euler18
 
+  #@tag :pending
   test "empty triangle sums zero" do
     assert Euler18.find_max_sum([[]]) == 0
   end
 
+  #@tag :pending
   test "one row triangle" do
     assert Euler18.find_max_sum([ [1234] ]) == 1234
   end
 
+  #@tag :pending
   test "two rows triangle" do
     triangle = [
       [1],
@@ -19,6 +22,7 @@ defmodule Euler18Test do
     assert Euler18.find_max_sum(triangle) == 4
   end
 
+  #@tag :pending
   test "two rows triangle (max in inverse order)" do
     triangle = [
       [1],
@@ -28,6 +32,7 @@ defmodule Euler18Test do
     assert Euler18.find_max_sum(triangle) == 4
   end
 
+  #@tag :pending
   test "three rows triangle" do
     triangle = [
       [1],
@@ -38,6 +43,7 @@ defmodule Euler18Test do
     assert Euler18.find_max_sum(triangle) == 10
   end
 
+  #@tag :pending
   test "three rows triangle (not descending order)" do
     triangle = [
       [1],
