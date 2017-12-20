c0736b6628852df730101677af43e63c217504c9
Rubén Chavarría <txingo@gmail.com>
2017-12-20 16:47:00 +0100

    Delete unused method

--- lib/euler18.ex
+++ lib/euler18.ex
@@ -4,16 +4,6 @@ defmodule Euler18 do
     reversed_sum(piramid |> Enum.reverse(), [])
   end
 
-  defp sum([], _col), do: 0
-  defp sum([[]], _col), do: 0
-  defp sum([ top | rows ], col) do
-    meaningful_cols = top |> Enum.drop(col) |> Enum.take(2)
-    max_top = meaningful_cols |> Enum.max()
-    col_of_max = meaningful_cols |> Enum.find_index(&(&1 == max_top))
-
-    max_top + sum(rows, col + col_of_max)
-  end
-
   defp reversed_sum([], previous_row), do: previous_row |> Enum.at(0)
   defp reversed_sum(piramid, previous_row) do
     [ current_row | rows ] = piramid
