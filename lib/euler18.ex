a04266dbc96fdbf0aa5a16ac402a37595d219e1f
Rubén Chavarría <txingo@gmail.com>
2017-12-20 17:09:28 +0100

    Import `Enum` to make code more concise

--- lib/euler18.ex
+++ lib/euler18.ex
@@ -1,11 +1,14 @@
 defmodule Euler18 do
 
+  # helps making the code sorter: you'll see `at(0)` instead of `Enum.at(0)`
+  import Enum, only: [reverse: 1, at: 2, drop: 2, take: 2, max: 1]
+
   def find_max_sum([]), do: 0
   def find_max_sum(piramid) do
-    sum(piramid |> Enum.reverse(), [])
+    sum(piramid |> reverse(), [])
   end
 
-  defp sum([], previous_row), do: previous_row |> Enum.at(0)
+  defp sum([], previous_row), do: previous_row |> at(0)
   defp sum(piramid, previous_row) do
     [ current_row | rows ] = piramid
 
@@ -16,10 +19,10 @@ defmodule Euler18 do
   defp merge_rows([], _previous_row), do: []
   defp merge_rows([ i | current_row ], previous_row) do
     max = previous_row |> max_of_first_two()
-    [ i + max | merge_rows(current_row, previous_row |> Enum.drop(1)) ]
+    [ i + max | merge_rows(current_row, previous_row |> drop(1)) ]
   end
 
   defp max_of_first_two([]), do: 0
-  defp max_of_first_two(row), do: row |> Enum.take(2) |> Enum.max()
+  defp max_of_first_two(row), do: row |> take(2) |> max()
 
 end
